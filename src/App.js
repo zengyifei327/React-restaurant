5bdc9c94c50c95a21b6f161367b4abf22b99fd88
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-09 14:49:50 -0800

    Add Testimonial section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Components/Home';
 import About from './Components/About'
 import Work from './Components/Work';
+import Testimonial from './Components/Testimonial';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Work />
+      <Testimonial />
     </div>
   );
 }
