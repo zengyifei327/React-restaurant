003af43becb7f86c2d86c2a9b4a211ae11b50bac
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-09 15:00:33 -0800

    Add footer section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './Components/Home';
 import About from './Components/About'
 import Work from './Components/Work';
 import Testimonial from './Components/Testimonial';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <About />
       <Work />
       <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
